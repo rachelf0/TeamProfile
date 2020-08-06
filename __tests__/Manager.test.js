3b7d557d0a3ae5cc1ee2859a317b8be54d34a940
Rachel Fritz <rachelfritz0@gmail.com>
2020-08-06 17:30:32 -0500

    added Intern and Manager tests to work correctly

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,19 @@
 const Manager = require("../lib/Manager");
+
+test('Set OfficeNumber with constructor argument', () => {
+    const testValue = 50;
+    const emp = new Manager("Amy", 1, "test@gmail.com", testValue);
+    expect(emp.officeNumber).toBe(testValue);
+});
+
+test('getRole() functions correctly', () => {
+    const testValue = "Manager";
+    const emp = new Manager("Amy", 1, "test@gmail.com");
+    expect(emp.getRole()).toBe(testValue);
+});
+
+test('Get Office Number with getOfficeNumber', () => {
+    const testValue = 50;
+    const emp = new Manager("Amy", 1, "test@gmail.com", testValue);
+    expect(emp.getOfficeNumber()).toBe(testValue);
+});
